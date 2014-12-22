7a99593165bd098e5fe1ae0b5d900baadcd6b204
Nem C <nemc@codeanvil.co>
2014-12-22 13:57:19 +0100

    mulitple updates

--- backend/vendor/ccomp/build/classes/classcompete/PropTeacher.php
+++ backend/vendor/ccomp/build/classes/classcompete/PropTeacher.php
@@ -1,11 +1,10 @@
 <?php
 
 
-
 /**
  * Skeleton subclass for representing a row from the 'teachers' table.
  *
- * 
+ *
  *
  * You should add additional methods to this class to meet the
  * application requirements.  This class will only be generated as
@@ -13,19 +12,31 @@
  *
  * @package    propel.generator.classcompete
  */
-class PropTeacher extends BasePropTeacher {
+class PropTeacher extends BasePropTeacher
+{
 
-    public function getCreatedAt(){
+    public function getCreatedAt()
+    {
         $db_time = parent::getCreated();
-        $new_time = date('m/d/Y h:i a', strtotime($db_time) - (-1 * AdminHelper::getTimezoneDiff()*60*60));
+        $new_time = date('m/d/Y h:i a', strtotime($db_time) - (-1 * AdminHelper::getTimezoneDiff() * 60 * 60));
         return $new_time;
     }
 
-    public function getUpdatedAt(){
+    public function getUpdatedAt()
+    {
         $db_time = parent::getModified();
-        $new_time = date('m/d/Y h:i a', strtotime($db_time) - (-1 * AdminHelper::getTimezoneDiff()*60*60));
+        $new_time = date('m/d/Y h:i a', strtotime($db_time) - (-1 * AdminHelper::getTimezoneDiff() * 60 * 60));
         return $new_time;
     }
 
+    public function getTotalPurchases()
+    {
+
+    }
+
+    public function getTotalQuantity()
+    {
+
+    }
 
 } // PropTeacher
